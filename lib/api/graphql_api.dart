0623fdbee8561f40372e139ed97e0559c771dd5a
FrankTN <franktn15@gmail.com>
2021-07-29 21:56:06 +0200

    Wrote possible queries from API as comment

--- lib/api/graphql_api.dart
+++ lib/api/graphql_api.dart
@@ -31,4 +31,89 @@ void main() async {
 
 }
 
+/*
+crowdaction(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+): CrowdactionGraph!
 
+crowdactionComment(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+): CrowdactionCommentGraph!
+
+crowdactionCommentCount(
+  crowdactionId: ID = null
+  status: CrowdactionCommentStatus = null
+): Int!
+
+crowdactionComments(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+  crowdactionId: ID = null
+  status: CrowdactionCommentStatus = null
+): [CrowdactionCommentGraph]
+
+crowdactionCount(
+  status: SearchCrowdactionStatus = null
+  category: Category = null
+): Int!
+
+crowdactions(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+  status: SearchCrowdactionStatus = nullcategory: Category = null
+): [CrowdactionGraph]
+
+currentUser(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+): ApplicationUserGraph
+
+misc: MiscellaneousGraph!
+
+stats: StatisticsGraph!
+
+user(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+): ApplicationUserGraph!
+
+userCount(
+  search: String = null
+): Int!
+
+users(
+  id: ID = null
+  ids: [ID] = null
+  orderBy: [OrderByGraph] = null
+  where: [WhereExpressionGraph] = null
+  skip: Int = null
+  take: Int = null
+  search: String = null
+): [ApplicationUserGraph]
+*/
